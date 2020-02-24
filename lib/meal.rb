af2be072e5210b69792d5c36c3b1b0353fd417e5
Micah Banschick <micha.ban@gmail.com>
2020-02-24 16:17:43 +0000

    Done.

--- lib/meal.rb
+++ lib/meal.rb
@@ -1,3 +1,24 @@
 class Meal
-
+  
+  attr_accessor :waiter, :customer, :total, :tip  
+  
+  @@all = []
+  
+  def initialize(waiter, customer, total, tip)
+    @waiter = waiter
+    @customer = customer 
+    @total = total 
+    @tip = tip 
+    customer.meals << self 
+    customer.waiters << waiter 
+    waiter.meals << self 
+    waiter.customers << customer 
+    waiter.tips << tip 
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,40 @@
-class Waiter
-
+class Waiter 
+  
+  attr_accessor :name, :exp, :meals, :customers, :tips  
+  
+  @@all = []
+  
+  def initialize(name, exp)
+    @name = name
+    @exp = exp 
+    @meals = []
+    @customers = []
+    @tips = []
+    @@all << self 
+  end 
+  
+  def self.all 
+    @@all 
+  end 
+  
+  def new_meal(customer,total,tip)
+    Meal.new(self,customer,total,tip)
+  end 
+  
+  def meals
+    @meals
+  end 
+  
+  def customers
+    @customers
+  end 
+  
+  def tips
+    @tips 
+  end 
+  
+  def best_tipper
+    @meals.filter{|meal| meal.tip == self.tips.max}[0].customer 
+  end 
+  
 end
